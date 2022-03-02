cff1f19b20dbf2c9940cecfb8b21fffa2a48f80c
yehang218 <yehang218@naver.com>
2022-03-03 02:50:24 +0900

    3052.c

--- Bronze/3052.c
+++ Bronze/3052.c
@@ -4,6 +4,8 @@ int main(void)
 {
     int num[10] = {0};
     int mod[10] = {0};
+    int check[10] = {0};
+    int result = 0;
 
     for(int i = 0; i< 10;i++){
         scanf("%d", &num[i]);
@@ -12,4 +14,20 @@ int main(void)
         mod[i] = num[i] % 42;
     }
     
+    for(int i =0; i< 10; i++)
+    {
+        for(int j = i + 1; j < 10; j++)
+        {
+            if(mod[i] == mod[j])
+                check[i]++;
+        }
+    }
+
+    for(int i = 0;i<10;i++)
+    {
+        if(check[i] == 0)
+            result++;
+    }
+    printf("%d", result);
+    return (0);
 }
