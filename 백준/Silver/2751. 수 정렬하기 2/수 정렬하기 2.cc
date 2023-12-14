f82ff40de9307d277f9af9fa5bc0b4f67a00a8c5
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2023-12-14 23:53:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 672 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int arr[1000001];
+
+int main(void)
+{
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+}
