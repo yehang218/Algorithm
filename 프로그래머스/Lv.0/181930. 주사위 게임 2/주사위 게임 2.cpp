90ec093762b194b2878459426dce4b885c72c280
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-01-15 14:12:30 +0900

    [level Lv.0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+	int answer = 0;
+
+	if (a != b && b != c && a != c)
+	{
+		answer = a + b + c;
+	}
+	else if ((a != b && b == c) || (b != c && c == a) || (a!= c && a == b))
+	{
+		answer = (a + b + c) * ((a * a) + (b * b) + (c * c));
+	}
+
+	else if (a == b && b == c)
+	{
+		answer = (a + b + c) * ((a * a) + (b * b) + (c * c)) * ((a * a * a) + (b * b * b) + (c * c * c));
+	}
+
+	return answer;
+}
