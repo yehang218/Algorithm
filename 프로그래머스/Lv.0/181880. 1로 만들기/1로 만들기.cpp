2c3c9f2e86935c5a5401b5b1344770cd2db710ab
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-01-16 13:50:48 +0900

    [level Lv.0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/Lv.0/181880. 1로 만들기/1로 만들기.cpp
@@ -4,23 +4,6 @@
 using namespace std;
 
 int solution(vector<int> num_list) {
-	int answer = 0;
-	int N;
-
-	for (int i = 0; i < num_list.size(); i++)
-	{
-		N = num_list[i];
-		while (N > 1)
-		{
-			if (N % 2 == 0) N = N / 2;
-			else if (N % 2 == 1)
-			{
-				N = N - 1;
-				N = N / 2;
-			}
-			answer++;
-		}
-	}
-
-	return answer;
+    int answer = 0;
+    return answer;
 }
