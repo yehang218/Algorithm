20e94973a869eaf5bd29baedcb04da92ec01c49e
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-01-16 14:06:05 +0900

    [level Lv.0] Title: 배열 만들기 2, Time: 5.14 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181921. 배열 만들기 2/배열 만들기 2.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int l, int r) {
+	vector<int> answer;
+
+	string strNum = "";
+	int check = 0;
+
+	for (int i = l; i <= r; i++)
+	{
+		strNum = to_string(i);
+		check = 0;
+
+		for (int j = 0; j < strNum.length(); j++)
+		{
+			if (strNum[j] == '5') check++;
+			else if (strNum[j] == '0') check++;
+		}
+
+		if (check == strNum.length()) {
+			answer.push_back(i);
+		}
+	}
+
+	if (answer.size() == 0) answer.push_back(-1);
+
+	return answer;
+}
