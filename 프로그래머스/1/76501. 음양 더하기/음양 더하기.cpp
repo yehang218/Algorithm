2f02b5be08f85034ddc73ceda4f870e491797ded
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-02-28 16:55:31 +0900

    [level 1] Title: 음양 더하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs)
+{
+    int answer = 0;
+
+    for (int i = 0; i < absolutes.size(); i++)
+    {
+        answer += signs[i] ? absolutes[i] : absolutes[i] * -1;
+    }
+
+    return answer;
+}
