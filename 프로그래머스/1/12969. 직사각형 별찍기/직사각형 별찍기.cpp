bb51535810c3a4c1b7288765a3adbedcffd88234
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-04-12 11:10:19 +0900

    [level 1] Title: 직사각형 별찍기, Time: 2.90 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.cpp
@@ -2,17 +2,10 @@
 
 using namespace std;
 
-int main(void)
-{
-    int n, m;
-    cin >> n >> m;
-
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-            cout << "*";
-        cout << "\n";
-    }
-
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a + b << endl;
     return 0;
 }
