a9a9c889458873a5645615e997868819b6d298aa
Seokhee Kim <69100145+yehang218@users.noreply.github.com>
2024-04-18 16:33:17 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/12982. 예산/예산.cpp
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -2,10 +2,25 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include<algorithm>
 
 using namespace std;
 
 int solution(vector<int> d, int budget) {
     int answer = 0;
+
+    sort(d.begin(), d.end());
+    
+    for(int i = 0;i < d.size(); i++)
+    {
+        budget = budget - d[i];
+        answer++;
+
+        if(budget < 0 )
+        {
+            answer--;
+            return answer;
+        }
+    }
     return answer;
 }
